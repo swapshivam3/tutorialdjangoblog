43511730d42bb5e71b4af6ddb43b3acb9a1afa19
swapshivam3 <swapshivam3@gmail.com>
2021-03-17 23:18:58 +0530

    ENV variable set

--- djangoTut/settings.py
+++ djangoTut/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1p#=v)qioyl^qx(qp5k#8(l1^!)e+9(kzi=nvo#1ce+^!qg+z%'
-
+# SECRET_KEY = '1p#=v)qioyl^qx(qp5k#8(l1^!)e+9(kzi=nvo#1ce+^!qg+z%'
+SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tutorialblogapp2.herokuapp.com']
 
 
 # Application definition
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles' )
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles' )
 
 
 
